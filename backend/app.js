940537e2ae1a0711862090ac6af1aa73e483e294
Prince <prince121kk@gmail.com>
2024-11-09 14:11:26 +0530

    changes

--- backend/app.js
+++ backend/app.js
@@ -48,7 +48,8 @@ const sessionOption={
     cookie:{
         expires:Date.now()+1000*60*60*24*7,
         maxAge:1000*60*60*24*7,
-        httpOnly:true
+        httpOnly:true,
+        sameSite: 'None',  
     }
 }
 app.use(session(sessionOption));
