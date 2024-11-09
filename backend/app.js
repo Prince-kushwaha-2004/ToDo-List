c626dc5c80f5a6636e5aad5357fba9ac261aed6f
Prince <prince121kk@gmail.com>
2024-11-09 21:39:08 +0530

    new change

--- backend/app.js
+++ backend/app.js
@@ -51,6 +51,7 @@ const sessionOption={
         expires:Date.now()+1000*60*60*24*7,
         maxAge:1000*60*60*24*7,
         secure: true,
+        httpOnly: true,
         sameSite: 'none'
     }
 }
